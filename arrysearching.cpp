8e994be5291f0eb1399a11dd71661eef1a2f4e40
Satyamkumar5783 <satyamK5783@gmail.com>
2024-03-11 23:08:05 +0530

    new code

---
+++ arrysearching.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+
+    for(int i=0; i<10; i++)
+    {
+
+    if(arr[i]==11)
+     {
+        cout<<"element found ";
+         break;
+         
+        
+     }
+     
+    }
+
+     
+    return 0;
+}
